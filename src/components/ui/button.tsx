ede3b5b4965ae3e4ccb91bc423035aac979858e4
eleo7 <souzaleonardo005@gmail.com>
2025-04-03 23:46:35 -0300

    fix: adiciona tipagem ao componente Button

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,3 +1,16 @@
-export function Button({ children, ...props }) {
-  return <button className="bg-blue-600 hover:bg-blue-700 text-white py-1 px-3 rounded" {...props}>{children}</button>;
+import { ButtonHTMLAttributes, DetailedHTMLProps, ReactNode } from 'react';
+
+type ButtonProps = DetailedHTMLProps<ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement> & {
+  children: ReactNode;
+};
+
+export function Button({ children, ...props }: ButtonProps) {
+  return (
+    <button
+      className="bg-blue-600 hover:bg-blue-700 text-white py-1 px-3 rounded"
+      {...props}
+    >
+      {children}
+    </button>
+  );
 }
