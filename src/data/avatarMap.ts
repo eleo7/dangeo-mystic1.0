52a97711d1f0d0b8d1b3275100bfc52dbc43433c
eleo7 <souzaleonardo005@gmail.com>
2025-04-04 02:09:24 -0300

    fix: adaf

--- src/data/avatarMap.ts
+++ src/data/avatarMap.ts
@@ -1,14 +1,14 @@
 export const avatarMap: Record<string, string[]> = {
   Aspectos: [
-    "/avatars/Nova pasta/Arus - Aspecto da Guerra.jpg",
-    "/avatars/Nova pasta/Chronarion - Aspecto do Tempo.jpg",
-    "/avatars/Nova pasta/Floranys - Aspecto da Vida.jpg",
-    "/avatars/Nova pasta/Goba - Aspecto da Lua.jpg",
-    "/avatars/Nova pasta/Mortalyon - Aspecto do Fim.jpg",
-    "/avatars/Nova pasta/Pyrion - Aspecto da Forja.jpg",
-    "/avatars/Nova pasta/Serenyss - Aspecto da Paz.jpg",
-    "/avatars/Nova pasta/Solari - Aspecto do Sol.jpg",
-    "/avatars/Nova pasta/Tempestade - Aspecto do Raio.jpg",
-    "/avatars/Nova pasta/Vox - Aspecto da Morte.jpg"
+    "/avatars/Nova pasta/Arus - Aspecto da Guerra.png",
+    "/avatars/Nova pasta/Chronarion - Aspecto do Tempo.png",
+    "/avatars/Nova pasta/Floranys - Aspecto da Vida.png",
+    "/avatars/Nova pasta/Goba - Aspecto da Lua.png",
+    "/avatars/Nova pasta/Mortalyon - Aspecto do Fim.png",
+    "/avatars/Nova pasta/Pyrion - Aspecto da Forja.png",
+    "/avatars/Nova pasta/Serenyss - Aspecto da Paz.png",
+    "/avatars/Nova pasta/Solari - Aspecto do Sol.png",
+    "/avatars/Nova pasta/Tempestade - Aspecto do Raio.png",
+    "/avatars/Nova pasta/Vox - Aspecto da Morte.png"
   ]
 };
