12c92204eb2fbdf38ec0646685b738ba224d4492
eleo7 <souzaleonardo005@gmail.com>
2025-04-03 23:16:29 -0300

    fix: limpa cache e reconfigura build do Next.js

--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"kmoTQGTyUWhgCrtiu8tNr1RK9/0HrxuoWSvw+EglFHk=\"\n}"
--- .next/static/development/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/create-character":["static\u002Fchunks\u002Fpages\u002Fcreate-character.js"],sortedPages:["\u002F_app","\u002F_error","\u002Fcreate-character"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
